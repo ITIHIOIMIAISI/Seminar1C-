b1e7e4f8760d63e12be6beef9ce9769e36b49cd7
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-08 01:27:15 +0300

    Решил Задание 6

---
+++ Domashka/Задание6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int nubmber = int.Parse(Console.ReadLine());
+if ((nubmber % 2) == 0)
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
