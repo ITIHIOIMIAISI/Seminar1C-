808de5ab56dca18d0b7c2e6f855f0ae291e23bd7
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-08 00:51:46 +0300

    Решил 2 Задание

---
+++ Domashka/Задание2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("a = ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int NumberB = int.Parse(Console.ReadLine());
+if(NumberA > NumberB)
+    Console.Write($"Max {NumberA}");
+else
+    Console.Write($"Max {NumberB}");
