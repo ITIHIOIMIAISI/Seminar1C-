6b5c8ebd4a534d40d4dea658d7f0eccb17a6897c
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-08 01:09:11 +0300

    Решил Задание 4

--- Domashka/Задание4/Program.cs
+++ Domashka/Задание4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("a = ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int NumberB = int.Parse(Console.ReadLine());
+Console.Write("с = ");
+int NumberC = int.Parse(Console.ReadLine());
+int max = NumberA;
+if(NumberB > max)
+    max = NumberB;
+    if(NumberC > max)
+        max = NumberC;
+Console.Write($"Max =  {max}");
