ee5f740da04b0f86d51d1e920f8e9fe2eb921744
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-14 13:34:19 +0300

    Задание 13 готово

---
+++ Domashka/Задание13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цыфру");
+int mainnum = int.Parse(Console.ReadLine());
+
+int num1 = mainnum % 10;
+int num2 = mainnum / 100;
+
+if (num2 == 0)
+    Console.WriteLine("В числе отсутствует 3 цифра");
+else
+    Console.WriteLine(num1);
