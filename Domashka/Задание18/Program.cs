454238ae602f64313ace6e8c9158fa145d7e752c
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-14 23:44:18 +0300

    Задание 18 решил как понял. Посмотрим, что сделал не так.

---
+++ Domashka/Задание18/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая по задонному номеру четвети, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.WriteLine("Enter a quarter to find the range of possible coordinates");
+int range = Convert.ToInt32(Console.ReadLine());
+
+if (range == 1)
+{    
+    Console.WriteLine("y = от 1 до 999 x = от 1 до 999");
+}
+if (range == 2)
+{    
+    Console.WriteLine("y = от 1 до 999 x = от -1 до -999");
+}
+if (range == 3)
+{    
+    Console.WriteLine("y = от -1 до -999 x = от -1 до -999");
+}
+if (range == 4)
+{    
+    Console.WriteLine("y = от -1 до -999 x = от 1 до 999");
+}
+else
+{
+    if (range == 0)
+{    
+    Console.WriteLine("There is no such quarter");
+}
+else
+{
+    Console.WriteLine("There is no such quarter");
+}
+}
