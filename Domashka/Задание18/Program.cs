b217faedc368c302c4b36426aae6e661a0a2c534
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-15 00:03:59 +0300

    Задание 18 Переелал на цикл switch оставил комментарии в коде.

--- Domashka/Задание18/Program.cs
+++ Domashka/Задание18/Program.cs
@@ -3,13 +3,34 @@
 Console.WriteLine("Enter a quarter to find the range of possible coordinates");
 int range = Convert.ToInt32(Console.ReadLine());
 
-if (range == 1)
+switch (range)
+{
+    case 1:
+    Console.WriteLine("y > 0 x > 0");
+    break;
+    case 2:
+    Console.WriteLine("y > 0 x < 0");
+    break;
+    case 3:
+    Console.WriteLine("y < 0 x < 0");
+    break;
+    case 4:
+    Console.WriteLine("y < 0 x > 0");
+    break;
+    default:
+        Console.WriteLine("There is no such quarter");
+         break;
+}
+// Хотел сделать switch но не стал из-за комментария к домашке
+// Не догадался записать как x y > или < 0
+
+/* if (range == 1)
 {    
     Console.WriteLine("y = от 1 до 999 x = от 1 до 999");
 }
 if (range == 2)
 {    
-    Console.WriteLine("y = от 1 до 999 x = от -1 до -999");
+    C
 }
 if (range == 3)
 {    
@@ -29,4 +50,4 @@ else
 {
     Console.WriteLine("There is no such quarter");
 }
-}
+} */
