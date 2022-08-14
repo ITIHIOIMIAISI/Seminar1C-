59b463e8c2d3094a7d2e72c5ba95951b8463c704
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-14 15:23:36 +0300

    Задание 10 готово

---
+++ Domashka/Задание10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter a three-digit number");
+
+int segment = int.Parse(Console.ReadLine());
+int segmentTwoNumber = segment / 10;
+int segmentOneNumber = segment / 100;
+
+int segmentFinal = (segmentTwoNumber - (segmentOneNumber * 10));
+Console.WriteLine($"Shows second digit: {segmentFinal}");
