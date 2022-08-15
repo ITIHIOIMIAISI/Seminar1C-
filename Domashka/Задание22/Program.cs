700fcdfb95298dba6dd6716660dc72a86d003665
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-16 00:16:46 +0300

    Задание 22 Готово

---
+++ Domashka/Задание22/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+//квадратов чисел от 1 до N.
+
+Console.WriteLine ("Введите число N :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; n >= i; i++)
+{
+    Console.Write(Math.Pow(i, 2) + ", ");
+}
