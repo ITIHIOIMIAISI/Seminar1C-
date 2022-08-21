dcf4eda31bfb754a15981cf6f5401cb1611b1aaa
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-21 23:23:34 +0300

    Здание 30 готово

---
+++ Domashka/Задание30/Program.cs
@@ -0,0 +1,15 @@
+﻿//=============Задача 30
+//Написать программу, которая ввыводит массив из 8 элементов,
+//заполненый нулями и единицыми вслучайном порядке.
+
+void RandArray()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+RandArray();
