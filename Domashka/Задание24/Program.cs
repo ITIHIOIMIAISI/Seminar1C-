274a65d47f83bf61962bbd1ba40c042f7fcc6350
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-21 23:22:38 +0300

    Здание 24 готово

---
+++ Domashka/Задание24/Program.cs
@@ -0,0 +1,17 @@
+﻿//==========Задача 24
+//Напишите программу, которая прнимает на входе число (А)
+// и выводит сумму чисел от 1 до (А)
+
+int SumNumN(int a)
+{
+int sum = 0;
+for (int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumN(a));
