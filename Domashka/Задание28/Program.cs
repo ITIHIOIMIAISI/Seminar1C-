a465852d1d2763165be1d9cafa3673ab28353613
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-21 23:23:18 +0300

    Здание 28 готово

---
+++ Domashka/Задание28/Program.cs
@@ -0,0 +1,17 @@
+﻿//========Задача 28
+// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N.
+
+int Factorial(int a)
+{
+    int f = 1;
+        for (int i = 2; i <= a; i++)
+        {
+            f *= i;
+        }
+        return f;
+}
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(N));
