7f63c04d58eb3a2b0722a249df023971cd081678
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-21 23:22:52 +0300

    Здание 25 готово

--- Domashka/Задание25/Program.cs
+++ Domashka/Задание25/Program.cs
@@ -2,17 +2,20 @@
 //Напишите цикл, который принимает на вход два числа 
 //(A и B) и возводит число A в натуральную степень B.
 
+int aNatDegB(int A, int B)
+{
+int sum = A;
+
+    for (int i = 1; i < B; i++)
+    {
+        sum = (sum * A);
+    }
+    return sum;
+}
 
 Console.WriteLine("Введицы число A");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введицы число B");
 int b = Convert.ToInt32(Console.ReadLine());
 
-int sum = 1;
-
-    for (int i = 0; i < b; i++)
-    {
-        sum = (sum * a);
-    }
-
-Console.Write($"Число {a} в натуральной степени {b} = {sum}");
+Console.Write(aNatDegB(a, b));
