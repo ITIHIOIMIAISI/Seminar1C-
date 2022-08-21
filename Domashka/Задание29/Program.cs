caa3727f53cd0b773cbabd88e6301c3bb5c7254f
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-21 23:23:26 +0300

    Здание 29 готово

---
+++ Domashka/Задание29/Program.cs
@@ -0,0 +1,22 @@
+﻿//=============Задача 29: 
+//Напишите программу, которая задаёт массив из N элементов, 
+//заполненных случайными числами из [a, b) и выводит их на экран.
+
+void abN(int a, int b, int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+Console.WriteLine("Введицы число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введицы число B");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введицы число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+abN(a,b,n);
