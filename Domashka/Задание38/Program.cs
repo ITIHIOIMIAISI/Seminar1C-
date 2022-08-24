61e5e69f6a2526e9e55cc9a6b3a8eabd9150686b
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-25 01:06:22 +0300

    Задание38 Переделал,работает с отрицательными числами .

--- Domashka/Задание38/Program.cs
+++ Domashka/Задание38/Program.cs
@@ -2,16 +2,17 @@
 //Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
 //элементов массива.
 
-void RandArray()
+void RandArray(int size, int minValue, int maxValue )
 {
-    float [] arr = new float [10];
+    float [] arr = new float [size];
     float min = 0;
     float max = 0;
     float diff = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(1, 100);
-        if (min == 0) min = arr[0]; 
+        arr[i] = new Random().Next(minValue, maxValue);
+        if (min == 0) min = arr[0];
+        if (max == 0) max = arr[0];
         if (max < arr[i]) max = arr[i];
         if (min > arr[i]) min = arr[i];
     }
@@ -19,4 +20,4 @@ void RandArray()
     Console.WriteLine($"[{String.Join(", ", arr)}] Разница между максимальным и минимальным числом:  {diff}");
 
 }
-RandArray();
+RandArray(12, -100, 101);
