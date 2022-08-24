a46467ccde0d65da90bfc4c69afbb4aff3df6201
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-24 20:40:01 +0300

    Задание31 Готово (Сам решил =) ) .

---
+++ Domashka/Задание31/Program.cs
@@ -0,0 +1,25 @@
+﻿//=========Задание 31
+// Задайте массив из 12 элементов, заполненный случайными числами промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов.
+
+void RandomArray()
+{
+    int[] arr = new int [12];
+    int negative = 0;
+    int positive = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 9);
+            if (arr[i] < 0)
+            {
+                negative += arr[i];
+            }
+            else
+            {
+                positive += arr[i];
+            }
+    }
+    Console.WriteLine($"[{String.Join(", ", arr)}]Сумма отрицательных чисел: {negative} Сумма положительных чисел: {positive}");
+}
+
+RandomArray();
